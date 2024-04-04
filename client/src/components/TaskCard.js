c68e8851e092c6537d7face044d8a1772ae69f25
kelcia253 <kelcia.antiuk@clinicorp.com>
2024-04-04 16:43:11 -0300

    Feat: Front Listar task

---
+++ client/src/components/TaskCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function TaskCard() {
+  return (
+    <div
+      className="task-card"
+      style={{
+        width: '80rem',
+        height: ' 100px',
+        backgroundColor: 'rgb(240, 240, 240)',
+        borderRadius: '8px',
+        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 2px 4px',
+        marginLeft: '2rem',
+        border: '1px solid purple',
+      }}
+    >
+      {/* Conteúdo do retângulo (opcional) */}
+    </div>
+  );
+}
+
+export default TaskCard;
