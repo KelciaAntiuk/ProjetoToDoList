6b71a573f1805a7facbd7e524ad9ef89004012ac
kelcia253 <kelcia.antiuk@clinicorp.com>
2024-04-12 13:34:24 -0300

    OJA-26: Revisar
    Identação

--- server/src/controllers/peopleController.js
+++ server/src/controllers/peopleController.js
@@ -1,37 +1,25 @@
 const peopleModels = require('../models/peopleModels');
 
 const getAll = async (_req,res) =>{
-
   const people = await peopleModels.getAll();
-
   return res.status(200).json(people);
-
 };
 
 const createPeople = async (req, res) =>{
-
   const createdPeople = await peopleModels.createPeople(req.body);
-
   return res.status(201).json(createdPeople)
-
 }
 
 const deletePeople = async (req, res) =>{
-
   const { id } = req.params;
-
   await peopleModels.deletePeople(id);
   return res.status(204).json();
-
-
 };
 
 const updatePeople = async (req, res) =>{
   const {id} = req.params;
-
   await peopleModels.updatePeople(id, req.body);
   return res.status(204).json();
-
 }
 
 module.exports = {
